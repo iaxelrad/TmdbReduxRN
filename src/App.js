7a5093e7396b2c742e8565c642599793618b01d4
Itamar Axelrad <itamara@aman.co.il>
2022-05-10 14:00:34 +0300

    Initial redux setup

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 import React from 'react';
+import {Provider} from 'react-redux';
 import RootNavigator from './navigation/RootNavigator';
+import {store} from './redux/store';
+
 const App = () => {
-  return <RootNavigator />;
+  return (
+    <Provider store={store}>
+      <RootNavigator />
+    </Provider>
+  );
 };
 export default App;
