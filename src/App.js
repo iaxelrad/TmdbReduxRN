4146e283dccb67fc06bbfdc04b9b469320c05e3e
Itamar Axelrad <itamara@aman.co.il>
2022-05-10 12:29:01 +0300

    Initial Structure Setup

---
+++ src/App.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import RootNavigator from './navigation/RootNavigator';
+const App = () => {
+  return <RootNavigator />;
+};
+export default App;
---
+++ src/navigation/RootNavigator.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+// import screens
+import Movies from '../screens/Movies';
+import Favorites from '../screens/Favorites';
+const Tab = createBottomTabNavigator();
+const tabBarOptions = {
+  headerShown: false,
+  tabBarShowLabel: false,
+  tabBarActiveTintColor: '#9381ff',
+  tabBarStyle: {
+    height: '10%',
+  },
+};
+const RootNavigator = () => {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator screenOptions={tabBarOptions}>
+        <Tab.Screen
+          name="Movies"
+          component={Movies}
+          options={{
+            tabBarIcon: ({color, size}) => (
+              <MaterialIcons name="movie-filter" color={color} size={size} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Favorites"
+          component={Favorites}
+          options={{
+            tabBarIcon: ({color, size}) => (
+              <MaterialIcons name="favorite" color={color} size={size} />
+            ),
+          }}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+};
+export default RootNavigator;
